26c83d5d38edb4a54334b86c3282119eb306beab
coderbot16 <coderbot16@gmail.com>
2018-05-23 15:48:45 -0700

    Move out b1.8.1+ code into separate crate

--- src/lib.rs
+++ src/lib.rs
@@ -17,16 +17,12 @@ pub mod rng;
 pub mod biome;
 pub mod sample;
 pub mod noise_field;
-// TODO: Implement decorators
-// Temporarily disable the decorator module for now.
-// They are not fully implemented, and we do not have a correct mechanism for the 4-chunk square yet.
 pub mod decorator;
 pub mod trig;
 pub mod structure;
 pub mod generator;
 pub mod distribution;
 pub mod segmented;
-pub mod image_ops;
 pub mod config;
 pub mod matcher;
 
